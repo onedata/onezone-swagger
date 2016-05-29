9f67ab079d50630c35c45bb6eeb37037735b6944
Bartosz Kryza <bkryza@gmail.com>
2016-05-29 17:43:37 +0200

    Added group paths

--- build.sh
+++ build.sh
@@ -6,6 +6,8 @@ docker run --rm -v `pwd`:/swagger docker.onedata.org/swagger-aggregator:1.4.0
 # Validate the JSON
 docker run --rm -v `pwd`:/swagger docker.onedata.org/swagger-cli:1.2.0 validate /swagger/swagger.json
 
+exit 0
+
 # Generate the Cowboy server stub
 docker run --rm -v `pwd`:/swagger -t docker.onedata.org/swagger-codegen:1.2.0 generate -i ./swagger.json -l cowboy -o ./generated/cowboy
 
