ece97d03c84411e330efbb161e161df43bf2435a
Bartosz Kryza <bkryza@gmail.com>
2016-07-27 13:23:35 +0200

    Added privileges and location headers

--- build.sh
+++ build.sh
@@ -4,10 +4,16 @@
 docker run --rm -e "CHOWNUID=${UID}" -v `pwd`:/swagger docker.onedata.org/swagger-aggregator:1.5.0
 
 # Validate the JSON
-docker run --rm -e "CHOWNUID=${UID}" -v `pwd`:/swagger docker.onedata.org/swagger-cli:1.5.0 validate /swagger/swagger.json
+output=$(docker run --rm -e "CHOWNUID=${UID}" -v `pwd`:/swagger docker.onedata.org/swagger-cli:1.5.0 validate /swagger/swagger.json 2>&1)
+
+if [[ $output =~ .*Swagger\ schema\ validation\ failed.* ]]; then
+  echo "Generated swagger.json is not valid - check YAML sources:\n"
+  echo $output
+  exit 1
+fi
 
 # Generate the Cowboy server stub
-docker run --rm -e "CHOWNUID=${UID}" -v `pwd`:/swagger -t docker.onedata.org/swagger-codegen:1.5.0 generate -i ./swagger.json -l cowboy -o ./generated/cowboy
+docker run --rm -e "CHOWNUID=${UID}" -v `pwd`:/swagger -t docker.onedata.org/swagger-codegen:1.5.3 generate -i ./swagger.json -l cowboy -o ./generated/cowboy
 
 # Generate C# stub to get all moustache tempalte keywords
 #swagger-codegen-dbg generate -i ./swagger.json -l csharp -o ./generated/csharp -o tmp > model.json
