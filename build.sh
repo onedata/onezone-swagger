b198c361ebf9c5cc927b84c1cf2224c79d8a84d4
Bartosz Kryza <bkryza@gmail.com>
2016-08-01 17:11:57 +0200

    Updated Erlang code generation

--- build.sh
+++ build.sh
@@ -15,6 +15,9 @@ fi
 # Generate the Cowboy server stub
 docker run --rm -e "CHOWNUID=${UID}" -v `pwd`:/swagger -t docker.onedata.org/swagger-codegen:1.5.3 generate -i ./swagger.json -l cowboy -o ./generated/cowboy
 
+# Fix generated Erlang files
+./fix_generated.py
+
 # Generate C# stub to get all moustache tempalte keywords
 #swagger-codegen-dbg generate -i ./swagger.json -l csharp -o ./generated/csharp -o tmp > model.json
 
