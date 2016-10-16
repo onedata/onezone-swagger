99202289e53efd0b389b1a04a828cb91772eb48c
Bartosz Kryza <bkryza@gmail.com>
2016-10-17 01:24:41 +0200

    Updated build.sh with bash client generation

--- build.sh
+++ build.sh
@@ -51,4 +51,8 @@ docker run --rm -v `pwd`:/swagger -t docker.onedata.org/swagger-gitbook:1.4.1 co
 #
 docker run --rm -e "CHOWNUID=${UID}" -v `pwd`:/swagger -t docker.onedata.org/swagger-codegen-official:ID-507bde287c  generate -i ./swagger.json -l python -o ./generated/python -c python-config.json
 
-# docker run --rm -e "CHOWNUID=${UID}" -v `pwd`/generated/python:/python -t python:2.7.12 /bin/bash -c "cd /python; python2 setup.py bdist_egg"
+#
+# Build Bash client
+#
+docker run --rm -e "CHOWNUID=${UID}" -v `pwd`:/swagger -t docker.onedata.org/bash-swagger-codegen:0.3.0 generate -i ./swagger.json -l bash -o ./generated/bash -c bash-config.json
+
